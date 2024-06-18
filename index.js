cd0948d1e655eb63815032178b2d8c44bd606f54
ChrisvanderHorst0308 <112861116+ChrisvanderHorst0308@users.noreply.github.com>
2024-06-18 19:34:21 +0200

    1

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.new-element .embedded-button').addEventListener('click', function() {
+        alert('Ingebed element actie!');
+    });
+});
